09379a62932f192eebe57910c2f64591275f6156
movedx <grytsenko.bogdan@gmail.com>
2021-03-21 20:12:01 +0100

    add dashboard, pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Preferences from "./components/Preferences/Preferences";
+import styles from "./App.css";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="wrapper" style={styles.wrapper}>
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/preferences">
+            <Preferences />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
