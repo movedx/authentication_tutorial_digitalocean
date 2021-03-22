ea629129152e44b62bab5f1610dd17fdfd954924
movedx <grytsenko.bogdan@gmail.com>
2021-03-22 01:24:32 +0100

    useToken custom hook

--- src/App.js
+++ src/App.js
@@ -3,19 +3,10 @@ import Dashboard from "./components/Dashboard/Dashboard";
 import Preferences from "./components/Preferences/Preferences";
 import Login from "./components/Login/Login";
 import styles from "./App.css";
-
-function setToken(userToken) {
-  sessionStorage.setItem("token", JSON.stringify(userToken));
-}
-
-function getToken() {
-  const tokenString = sessionStorage.getItem("token");
-  const userToken = JSON.parse(tokenString);
-  return userToken?.token;
-}
+import useToken from "./components/Hooks/useToken";
 
 function App() {
-  const token = getToken();
+  const { token, setToken } = useToken();
 
   if (!token) {
     return <Login setToken={setToken} />;
